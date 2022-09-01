0233866491a2723bd2b57b011232d82c17458d23
NasirMuh <octobernine1995@gmail.com>
2022-09-01 22:09:27 +0500

    back to normal

--- pages/api/test/[ClientId].js
+++ pages/api/test/[ClientId].js
@@ -1,31 +1,31 @@
-// import connectMongo from "../../../utils/connectMongo";
-// import Test from "../../../models/testModel"
+import connectMongo from "../../../utils/connectMongo";
+import Test from "../../../models/testModel"
 
-// export default async function handler(req, res) {
-//     const { method } = req;
-//     const { ClientId } = req.query;
-//     switch (method) {
-//         case "PUT":
-//             try {
-//                 const { name, email } = req.body;
-//                 await connectMongo()
-//                 await Test.updateOne({ _id: ClientId }, { name, email })
-//                 res.status(200).json({ success: true })
-//             } catch (error) {
-//                 console.log(error)
-//                 res.status(500).json({ success: false, error })
-//             }
-//             break;
+export default async function handler(req, res) {
+    const { method } = req;
+    const { ClientId } = req.query;
+    switch (method) {
+        case "PUT":
+            try {
+                const { name, email } = req.body;
+                await connectMongo()
+                await Test.updateOne({ _id: ClientId }, { name, email })
+                res.status(200).json({ success: true })
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({ success: false, error })
+            }
+            break;
 
-//         case "DELETE":
-//             try {
-//                 await connectMongo()
-//                 await Test.deleteOne({ _id: ClientId })
-//                 res.status(200).json({ success: true })
-//             } catch (error) {
-//                 console.log(error)
-//                 res.status(500).json({ success: false, error })
-//             }
-//             break;
-//     }
-// }
+        case "DELETE":
+            try {
+                await connectMongo()
+                await Test.deleteOne({ _id: ClientId })
+                res.status(200).json({ success: true })
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({ success: false, error })
+            }
+            break;
+    }
+}
