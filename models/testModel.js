5b8f433109412542fca3afc74c4186a3af94d038
NasirMuh <octobernine1995@gmail.com>
2022-09-01 21:43:15 +0500

    commenting all mongo data

--- models/testModel.js
+++ models/testModel.js
@@ -1,13 +1,13 @@
-import { Schema, model, models } from "mongoose";
+// import { Schema, model, models } from "mongoose";
 
-const testSchema = new Schema({
-    name: String,
-    email: String
-})
+// const testSchema = new Schema({
+//     name: String,
+//     email: String
+// })
 
-const Test = models.Test || model('Test', testSchema)
+// const Test = models.Test || model('Test', testSchema)
 
-export default Test;
+// export default Test;
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
 import Head from 'next/head'
-import { useState } from 'react'
+// import { useState } from 'react'
 import styles from '../styles/Home.module.css'
-import Test from "../models/testModel"
-import connectMongo from '../utils/connectMongo';
+// import Test from "../models/testModel"
+// import connectMongo from '../utils/connectMongo';
 
 export default function Home({ test }) {
 
-  const [userData, setUserData] = useState(test)
-  const createTest = async () => {
-    const rondomNumber = Math.floor(Math.random() * 1000)
-    const res = await fetch("/api/test", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: `Test  ${rondomNumber}`,
-        email: `Nasir ${rondomNumber}@test.com`
-      })
-    })
-    const data = await res.json()
-  }
-  const DeleteData = async (_id) => {
-    const res = await fetch(`/api/test/${_id}`, {
-      method: "DELETE"
-    })
-    const data = await res.json()
-  }
+  // const [userData, setUserData] = useState(test)
+  // const createTest = async () => {
+  //   const rondomNumber = Math.floor(Math.random() * 1000)
+  //   const res = await fetch("/api/test", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       name: `Test  ${rondomNumber}`,
+  //       email: `Nasir ${rondomNumber}@test.com`
+  //     })
+  //   })
+  //   const data = await res.json()
+  // }
+  // const DeleteData = async (_id) => {
+  //   const res = await fetch(`/api/test/${_id}`, {
+  //     method: "DELETE"
+  //   })
+  //   const data = await res.json()
+  // }
 
   return (
     <div className={styles.container}>
@@ -40,7 +40,7 @@ export default function Home({ test }) {
         <h1 className={styles.title}>
           Welcome to Next.js And Mongoose
         </h1>
-        <div className={styles.grid}>
+        {/* <div className={styles.grid}>
           {
             userData?.map((userData) => {
               return <div key={userData._id} className={styles.card} onClick={() => DeleteData(userData._id)}>
@@ -49,22 +49,22 @@ export default function Home({ test }) {
               </div>
             })
           }
-        </div>
-        <button onClick={createTest}>Create </button>
+        </div> */}
+        {/* <button onClick={createTest}>Create </button> */}
       </main>
     </div>
   )
 }
-export const getServerSideProps = async () => {
-  try {
-    await connectMongo()
-    const test = await Test.find()
-    return {
-      props: {
-        test: JSON.parse(JSON.stringify(test))
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
+// export const getServerSideProps = async () => {
+//   try {
+//     await connectMongo()
+//     const test = await Test.find()
+//     return {
+//       props: {
+//         test: JSON.parse(JSON.stringify(test))
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
